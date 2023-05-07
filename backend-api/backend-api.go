9fd130585c9f5ab05e60478d37e9b8c1caee45dd
Li Dong <cnlidong@live.cn>
2023-05-07 22:28:16 +0800

    修正转发header错误

--- backend-api/backend-api.go
+++ backend-api/backend-api.go
@@ -22,6 +22,7 @@ func init() {
 		// 替换 r.URL.Path 中的 /api 为 /backend-api
 		// 例如：/api/auth/session -> /backend-api/auth/session
 		req.URL.Path = "/backend-api" + req.URL.Path[4:]
+		req.Host = u.Host // 替换请求头中的 Host 为 API_PROXY 的 Host
 		g.Log().Debug(r.GetCtx(), "proxy to "+u.String()+req.URL.Path)
 
 		proxy.ServeHTTP(r.Response.Writer.RawWriter(), r.Request)
